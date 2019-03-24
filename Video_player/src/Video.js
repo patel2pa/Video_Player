c68db10cbb58059b457464f73bd8f21af877cd15
patel2pa <44906574+patel2pa@users.noreply.github.com>
2019-03-23 23:34:13 -0400

    Added Video player program

---
+++ Video_player/src/Video.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class Video extends React.Component {
+  render() {
+    return (
+      <div>
+        <video controls autostart autoplay 
+        src = {this.props.src} />
+      </div>
+    );
+  }
+}
+
+export default Video;
